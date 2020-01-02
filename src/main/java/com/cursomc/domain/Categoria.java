63ba5fd9958b68375afab29f441f8e502a128743
willianbello <willianbello.s@gmail.com>
2020-01-02 02:59:20 -0300

    Criando repository e service para Categoria

--- src/main/java/com/cursomc/domain/Categoria.java
+++ src/main/java/com/cursomc/domain/Categoria.java
@@ -6,8 +6,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="TB_CATEGORIA")
 public class Categoria implements Serializable{
 
 	private static final long serialVersionUID = 1L;
