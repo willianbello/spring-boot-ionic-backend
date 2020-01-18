a5e4bd164cb4b3250b8f09493be9b4e02fea3972
willianbello <willianbello.s@gmail.com>
2020-01-18 19:18:54 -0300

    Implementando Autorização

--- src/main/java/com/cursomc/config/SecurityConfig.java
+++ src/main/java/com/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.cursomc.security.JWTAuthenticationFilter;
+import com.cursomc.security.JWTAuthorizationFilter;
 import com.cursomc.security.JWTUtil;
 
 @Configuration
@@ -69,6 +70,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.authenticated();
 		
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		
 		http
 			.sessionManagement()
